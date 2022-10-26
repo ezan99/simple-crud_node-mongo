5b04d07e7ccaa65e159a82d7721684fa0cf67e2b
IljaziEzan <ezan.iljazi@drweb.mk>
2022-10-26 15:38:22 +0200

    minor change regarding port number

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(4500, () => {
+app.listen(process.env.PORT || 4500, () => {
     console.log(`Server Started at ${4500}`)
 })
